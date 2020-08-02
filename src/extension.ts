287a2254f1497708dc100c91d8dbbae9cd9bc602
Namal Dayarathna <dayarathnand@gmail.com>
2020-08-02 13:46:21 +0100

    Add command to open host file from path in config

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hosts-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hosts Helper!');
+	let disposable = vscode.commands.registerCommand('hosts-helper.helloWorld', async () => {
+		const path = vscode.workspace.getConfiguration('hostHelper').get('path', '');
+		
+		// Check file exists
+		fs.access(path, fs.constants.F_OK, async err => {
+			if (err) {
+				vscode.window.showErrorMessage("Host file cannot be read, please check the path in configuration");
+			}
+			else {
+				// Convert path to VS Code URI
+				const hostUri = vscode.Uri.file(path);
+				// Show the host file
+				await vscode.window.showTextDocument(hostUri);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
